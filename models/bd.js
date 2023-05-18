4ed38cf2f4c858fb2dfa36dfa9b90603dac31abf
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-05-17 22:07:31 -0300

    teste

--- models/bd.js
+++ models/bd.js
@@ -1,20 +1,3 @@
-// const Sequelize = require('sequelize');
-// const sequelize = new Sequelize("bd-oliveira", "gwdkoj05746cfhnh74c2", "pscale_pw_x9A6wchx4HtQTk0IOcfEazSLIB1hGV4aPqbqPl1cmJJ", {
-//     host: "aws.connect.psdb.cloud",
-//     dialect: "mysql",
-//     ssl: true,
-//     dialectOptions: {
-//       ssl: 'Amazon RDS',
-//       rejectUnauthorized: false
-//     }
-//   });
-  
-
-// sequelize.authenticate().then(function(){
-//     console.log("sucesso ao se conectar");
-// }).catch(function(err){
-//     console.log("erro ao conectar", err);
-// });
 
 const mysql = require('mysql2');
 require('dotenv').config();
