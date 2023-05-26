6b882febb85c335f08d5178ddcc06d5a63110c94
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-05-26 20:42:33 -0300

    Adicionar gastos

--- controller/gastos/gastosCartao.js
+++ controller/gastos/gastosCartao.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 const key = '$2y$10MFKDgDBujKwY.VZi/DH6JuR58ISGjlS6mlEobHlmhX9zQ.Ha4c3qC2';
 const moment = require('moment');
 
-function FaturaCaro(req, res) {
+
+async function FaturaCaro(req, res) {
     const { gastos, userID } = req.query;
     if (!gastos || typeof gastos !== 'string' || gastos.trim() === '') {
         return res.status(501).json({ message: 'Nenhum dado entregue.' });
@@ -23,40 +24,47 @@ function FaturaCaro(req, res) {
         return res.status(501).json({ message: 'Nenhum dado de compra encontrado.' });
     }
 
-    const sqlCheck = 'SELECT * FROM compras_cartao WHERE compra_nome = ? AND compra_data = ? AND compra_hora = ? AND compra_valor = ? AND compra_status = ? AND id_user = ?';
+    const sqlCheck = 'SELECT * FROM compras_cartao WHERE compra_nome = ? AND compra_data = ? AND compra_hora = ? AND compra_valor = ? AND id_user = ?';
     const sqlInsert = 'INSERT INTO compras_cartao (compra_nome, compra_data, compra_hora, compra_valor, compra_status, id_user) VALUES (?, ?, ?, ?, ?, ?)';
 
-    compras.forEach((compra) => {
-        const statusValue = (compra.status === 'confirmada' || compra.status === 'aprovada') ? 1 : 0;
-        const dataFormatted = moment(compra.data, 'DD/MM/YYYY').format('YYYY-MM-DD');
-        const valorFormatted = compra.valor.replace(',', '.');
+    try {
+        for (const compra of compras) {
+            const statusValue = (compra.status === 'confirmada' || compra.status === 'aprovada') ? 1 : 0;
+            const dataFormatted = moment(compra.data, 'DD/MM/YYYY').format('YYYY-MM-DD');
+            const valorFormatted = compra.valor.replace(',', '.');
 
-        const valuesCheck = [compra.nome, dataFormatted, compra.hora, valorFormatted, compra.status, userID];
-        connection.query(sqlCheck, valuesCheck, (err, result) => {
-            if (err) {
-                console.error('Erro ao verificar os dados no banco de dados:', err);
+            const valuesCheck = [compra.nome, dataFormatted, compra.hora, valorFormatted, userID];
+            const result = await executeQuery(sqlCheck, valuesCheck);
+
+            if (result.length === 0) {
+                const valuesInsert = [compra.nome, dataFormatted, compra.hora, valorFormatted, statusValue, userID];
+                await executeQuery(sqlInsert, valuesInsert);
+                console.log('Dados salvos com sucesso.');
             } else {
-                if (result.length === 0) {
-                    const valuesInsert = [compra.nome, dataFormatted, compra.hora, valorFormatted, statusValue, userID];
-                    connection.query(sqlInsert, valuesInsert, (err, result) => {
-                        if (err) {
-                            console.error('Erro ao inserir os dados no banco de dados:', err);
-                            return res.status(501).json({ message: 'não foi possivel guardar os dados ' });
-                            // Trate o erro conforme necessário
-                        } else {
-                            console.log('Dados salvos com sucesso.');
-                            return res.status(201).json({ message: 'Dados salvos com sucesso.' });
-                            // Faça algo após a inserção bem-sucedida
-                        }
-                    });
-                } else {
-                    console.log('Compra já existente, ignorando inserção.');
-                }
+                console.log('Compra já existente, ignorando inserção.');
+            }
+        }
+
+        return res.status(201).json({ message: 'Dados salvos com sucesso.' });
+    } catch (error) {
+        console.error('Erro ao inserir os dados no banco de dados:', error);
+        return res.status(501).json({ message: 'Erro ao inserir os dados no banco de dados.' });
+    }
+}
+
+function executeQuery(sql, values) {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, values, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result);
             }
         });
     });
-
 }
+
+
 
 function buscarGastosUsuario(req, res) {
     const { userID } = req.query;
