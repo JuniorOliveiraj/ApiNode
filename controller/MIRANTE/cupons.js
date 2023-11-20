a8dca9c3a752aca5a0fcc72329e33da0812517ce
junior de oliveira belem <junioroliveira.belem@gmail.com>
2023-11-19 22:09:49 -0300

    order by DESC

--- controller/MIRANTE/cupons.js
+++ controller/MIRANTE/cupons.js
@@ -38,7 +38,7 @@ async function fetchData(req, res) {
 async function ListCupons(req, res) {
     try {
         const currentDate = new Date().toISOString().split('T')[0];
-        const sql = "select id, nome , data_por_dia as 'date' ,  usus as amount, status   from  Mirante_cupons   ORDER BY amount";
+        const sql = "select id, nome , data_por_dia as 'date' ,  usus as amount, status   from  Mirante_cupons   ORDER BY amount DESC";
         const data = await executeQuery(sql, [currentDate]);
 
         return res.status(200).json({ mensagem: 'sucesso', dados: data, currentDate:currentDate });
