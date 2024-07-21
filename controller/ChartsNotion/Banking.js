09b4c120a9ebb0f35a76be10588f071625877f38
Junior Mac <junioroliveira.belem@gmail.com>
2024-07-21 14:17:57 -0300

    Gastos Banking

--- controller/ChartsNotion/Banking.js
+++ controller/ChartsNotion/Banking.js
@@ -13,13 +13,13 @@ const SaldoEmConta = async (req, res) => {
         MONTH(data) = MONTH(CURDATE());`;
     query2 =  `
         SELECT 
-        SUM(CASE WHEN DAYOFWEEK(data) = 1 THEN valor ELSE 0 END) AS Domingo,
         SUM(CASE WHEN DAYOFWEEK(data) = 2 THEN valor ELSE 0 END) AS Segunda_feira,
         SUM(CASE WHEN DAYOFWEEK(data) = 3 THEN valor ELSE 0 END) AS Terca_feira,
         SUM(CASE WHEN DAYOFWEEK(data) = 4 THEN valor ELSE 0 END) AS Quarta_feira,
         SUM(CASE WHEN DAYOFWEEK(data) = 5 THEN valor ELSE 0 END) AS Quinta_feira,
         SUM(CASE WHEN DAYOFWEEK(data) = 6 THEN valor ELSE 0 END) AS Sexta_feira,
-        SUM(CASE WHEN DAYOFWEEK(data) = 7 THEN valor ELSE 0 END) AS Sabado
+        SUM(CASE WHEN DAYOFWEEK(data) = 7 THEN valor ELSE 0 END) AS Sabado,
+        SUM(CASE WHEN DAYOFWEEK(data) = 1 THEN valor ELSE 0 END) AS Domingo
 
         FROM 
             gastos_mensais_notion
@@ -30,7 +30,8 @@ const SaldoEmConta = async (req, res) => {
     const result2 = await executeQuery(query2, []);
     if (result && result2) {
         const chartValues = Object.values(result2[0]);
-        return res.status(200).json({ mensagem: 'ok', values:result, charts:chartValues });
+        const chartKeys = Object.keys(result2[0]);
+        return res.status(200).json({ mensagem: 'ok', values:result, charts:chartValues, week:chartKeys });
     } else {
         return res.status(401).json({ error: 'sem dados na query ou erro interno' });
     }
